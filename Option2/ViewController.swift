bfa68f5053d3f43976ed894d3ce4f28b135a5ff4
Ryuma.h <ryuma810dragon@gmail.com>
2019-05-31 13:44:11 +0900

    Initial Commit

---
+++ Option2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Option2
+//
+//  Created by Ryuma Harada on 2019/05/31.
+//  Copyright © 2019 Ryuma Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
